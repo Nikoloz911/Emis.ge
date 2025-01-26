23330829ba692041501d3366ab128baeb5c595f4
nika <nikalobjanidze014@gmail.com>
2025-01-26 18:30:55 +0400

    Changed Carousel images. Added Images Not Image Links.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -114,7 +114,6 @@ private loadStats(): void {
 scrollLeftLink(containerSelector: string, cardSelector: string): void {
   let container = document.querySelector(containerSelector) as HTMLElement;
   let cardWidth = container.querySelector(cardSelector)?.clientWidth || 0;
-
   container.scrollTo({
     left: container.scrollLeft - cardWidth,
     behavior: 'smooth'
@@ -126,7 +125,6 @@ scrollLeftLink(containerSelector: string, cardSelector: string): void {
     }
   }, 300);
 }
-
 scrollRightLink(containerSelector: string, cardSelector: string): void {
   let container = document.querySelector(containerSelector) as HTMLElement;
   let cardWidth = container.querySelector(cardSelector)?.clientWidth || 0;
@@ -142,7 +140,6 @@ scrollRightLink(containerSelector: string, cardSelector: string): void {
     }
   }, 300);
 }
-
 scrollLeftPartners() {
   this.scrollLeftLink('.partners-products-container', '.partners-card');
 }
@@ -163,48 +160,34 @@ onMouseDownScroll(event: MouseEvent) {
   this.isDragging = true;
   this.startX = event.pageX - (event.currentTarget as HTMLElement).offsetLeft;
   this.scrollLeft = (event.currentTarget as HTMLElement).scrollLeft;
-  
   let card = event.target as HTMLElement;
   let cardElement = card.closest('.products-card') as HTMLElement;
-
   if (cardElement) {
-   
-    if (this.previousCard && this.previousCard !== cardElement) {
+   if (this.previousCard && this.previousCard !== cardElement) {
       this.previousCard.style.border = '';
     }
-
     cardElement.style.border = '1px solid blue';
     this.previousCard = cardElement;
   }
-
   (event.currentTarget as HTMLElement).classList.add('dragging'); 
-
- 
   event.preventDefault();
 }
-
-
 onMouseMoveScroll(event: MouseEvent): void {
   if (!this.isDragging) return;
-
   let container = event.currentTarget as HTMLElement;
   let x = event.pageX - container.offsetLeft;
   let walk = (x - this.startX) * 2; 
   container.scrollLeft = this.scrollLeft - walk;
-
   event.preventDefault();
 }
-
 onMouseUpScroll(event: MouseEvent): void {
   this.isDragging = false;
   (event.currentTarget as HTMLElement).classList.remove('dragging');
-
   if (this.previousCard) {
     this.previousCard.style.border = '';
   }
   this.previousCard = null;
 }
-
 onMouseLeaveScroll(event: MouseEvent): void {
   this.isDragging = false;
   (event.currentTarget as HTMLElement).classList.remove('dragging');
@@ -214,7 +197,6 @@ onMouseLeaveScroll(event: MouseEvent): void {
   }
   this.previousCard = null;
 }
-
 onClickCard(event: MouseEvent): void {
  
   if (this.isDragging) {
@@ -222,14 +204,10 @@ onClickCard(event: MouseEvent): void {
   }
 }
 /// PRODUCT CARDS SCROLL ///
-
   /// TOOTLIP ON MOUSE ON HOME PAGE PRODUCTS CARDS ///
   onMouseEnter(event: MouseEvent) {
-    let card = (event.currentTarget as HTMLElement);
-    
-   
+    let card = (event.currentTarget as HTMLElement);  
     clearTimeout(this.tooltipTimer);
-
     let tooltip = card.querySelector('.tooltip') as HTMLElement;
     if (tooltip) {
       this.tooltipVisible = true;
@@ -238,13 +216,9 @@ onClickCard(event: MouseEvent): void {
       tooltip.style.visibility = 'visible';
       tooltip.style.transition = 'opacity 0.9s ease-in-out, visibility 0.9s linear 0.9s'; 
     }
-
-   
     card.addEventListener('mousemove', this.onMouseMove.bind(this));
     card.addEventListener('mouseleave', this.onMouseLeave.bind(this));
   }
-
- 
   onMouseLeave(event: MouseEvent) {
     clearTimeout(this.tooltipTimer);
     let card = (event.currentTarget as HTMLElement);
@@ -258,8 +232,6 @@ onClickCard(event: MouseEvent): void {
     card.removeEventListener('mousemove', this.onMouseMove.bind(this));
     card.removeEventListener('mouseleave', this.onMouseLeave.bind(this));
   }
-
-  
   onMouseMove(event: MouseEvent) {
     let card = event.currentTarget as HTMLElement;
     let tooltip = card.querySelector('.tooltip') as HTMLElement;
@@ -274,19 +246,18 @@ onClickCard(event: MouseEvent): void {
   }
   /// TOOTLIP ON MOUSE ON HOME PAGE PRODUCTS CARDS ///
 
-  
   /// HOME PAGE CAROUSEL ///
   items = [
-    { id: 108, image: 'https://emis-api.emis.ge/storage/media/1291/%E1%83%93%E1%83%98%E1%83%93%E1%83%98.png', text: 'ცხადდება რიგგარეშე მობილობა აბიტურიენტებისა და მობილობის მსურველი იმ პირებისათვის, რომლებმაც, 2024-2025 სასწავლო წლისათვის, ჩარიცხვის უფლება მოიპოვეს შპს დავით აღმაშენებლის სახელობის უნივერსიტეტში.' }, 
-    { id: 107, image: 'https://emis-api.emis.ge/storage/media/1288/%E1%83%A1%E1%83%98%E1%83%90%E1%83%AE%E1%83%9A%E1%83%94%E1%83%94%E1%83%91%E1%83%98-2024-Recovered.png', text: 'სახელოვნებო-შემოქმედებით ან სასპორტო საგანმანათლებლო პროგრამებზე მობილობაში მონაწილე სტუდენტების საყურადღებოდ!' }, 
-    { id: 106, image: 'https://emis-api.emis.ge/storage/media/1289/vet.emis.ge.png', text: 'პროფესიულ საგანმანათლებლო პროგრამებზე რეგისტრაციის მსურველთათვის'},
-    { id: 104, image: 'https://emis-api.emis.ge/storage/media/1272/%E1%83%A2%E1%83%A0%E1%83%90%E1%83%9F%E1%83%98%E1%83%A0%E1%83%94%E1%83%91%E1%83%90-%E1%83%93%E1%83%98%E1%83%93%E1%83%98.webp', text: 'კურსდამთავრებულთა სტაჟირების პროგრამის ფარგლებში განაცხადების მიღება იწყება' },
-    { id: 103, image: 'https://emis-api.emis.ge/storage/media/1274/%E1%83%9B%E1%83%9D%E1%83%91%E1%83%98%E1%83%9A%E1%83%9D%E1%83%91%E1%83%98%E1%83%A1-%E1%83%95%E1%83%90%E1%83%99%E1%83%90%E1%83%9C%E1%83%A2%E1%83%A3%E1%83%A0%E1%83%98-%E1%83%90%E1%83%93%E1%83%92%E1%83%98%E1%83%9A%E1%83%94%E1%83%91%E1%83%98!.webp', text: 'მობილობის ვაკანტური ადგილები!' },
-    { id: 101, image: 'https://emis-api.emis.ge/storage/media/1278/mobiloba-3.webp', text: 'სტუდენტთა მობილობა' },
-    { id: 102, image: 'https://emis-api.emis.ge/storage/media/1276/Screenshot-2024-07-15-171247-(1)-(1).webp', text: 'გამოცხადდა მიღება 2024-2025 სასწავლო წლისთვის მასწავლებლის მომზადების საგანმანათლებლო პროგრამაზე' }, 
-    { id: 97, image: 'https://emis-api.emis.ge/storage/media/1158/jpeg-optimizer-students-knowing-right-answer-664af7b1c506c-1_1075x574-(1).webp', text: 'საჯარო სკოლებში პირველკლასელთა რეგისტრაცია იწყება – 2024-2025' },
-    { id: 96, image: 'https://emis-api.emis.ge/storage/media/1105/1674393763-lid11bbv-1-664af7a2dd7c1.webp', text: 'საჯარო სკოლის დირექტორების კონკურსი' },
-    { id: 80, image: 'https://emis-api.emis.ge/storage/media/1133/kids-using-laptop-school-23-2148666211-664af7b4f15de-(1).webp', text: 'გაიგეთ როგორ გამოიყენოთ საბავშვო ბუკები' }, 
+    { id: 108, image: 'assets/meeqvse.png', text: 'ცხადდება რიგგარეშე მობილობა აბიტურიენტებისა და მობილობის მსურველი იმ პირებისათვის, რომლებმაც, 2024-2025 სასწავლო წლისათვის, ჩარიცხვის უფლება მოიპოვეს შპს დავით აღმაშენებლის სახელობის უნივერსიტეტში.' }, 
+    { id: 107, image: 'assets/meshvide.png', text: 'სახელოვნებო-შემოქმედებით ან სასპორტო საგანმანათლებლო პროგრამებზე მობილობაში მონაწილე სტუდენტების საყურადღებოდ!' }, 
+    { id: 106, image: 'assets/merve.png', text: 'პროფესიულ საგანმანათლებლო პროგრამებზე რეგისტრაციის მსურველთათვის'},
+    { id: 104, image: 'assets/mecxre.webp', text: 'კურსდამთავრებულთა სტაჟირების პროგრამის ფარგლებში განაცხადების მიღება იწყება' },
+    { id: 103, image: 'assets/meate.webp', text: 'მობილობის ვაკანტური ადგილები!' },
+    { id: 101, image: 'assets/pirveli.png', text: 'სტუდენტთა მობილობა' },
+    { id: 102, image: 'assets/meore.webp', text: 'გამოცხადდა მიღება 2024-2025 სასწავლო წლისთვის მასწავლებლის მომზადების საგანმანათლებლო პროგრამაზე' }, 
+    { id: 97, image: 'assets/mesame.webp', text: 'საჯარო სკოლებში პირველკლასელთა რეგისტრაცია იწყება – 2024-2025' },
+    { id: 96, image: 'assets/meotxe.webp', text: 'საჯარო სკოლის დირექტორების კონკურსი' },
+    { id: 80, image: 'assets/mexute.webp', text: 'გაიგეთ როგორ გამოიყენოთ საბავშვო ბუკები' }, 
   ];
   currentIndex = 0;
   autoSlideSubscription: Subscription | undefined;
